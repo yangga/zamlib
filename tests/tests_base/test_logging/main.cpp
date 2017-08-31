a775b7a62125276b5f08e57593fcc17a3d838b66
yangga <yangga0070@gmail.com>
2017-08-31 19:27:52 +0900

    added test logging project

--- tests/tests_base/test_logging/main.cpp
+++ tests/tests_base/test_logging/main.cpp
@@ -2,3 +2,13 @@
 // Created by SungWoo Yang on 2017. 8. 31..
 //
 
+#include <iostream>
+
+#include <base/log/logger.h>
+
+using namespace zam::base::log;
+
+int main(int argc, char* argv[]) {
+    logger::debug("test", "hello world");
+    return 0;
+}
