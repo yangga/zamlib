03898650a2f24c201f828b7d470bf85820dea22c
yangga <yangga0070@gmail.com>
2017-09-10 12:30:33 +0900

    test

--- cmake/MSVCCFlagOverride.cmake
+++ cmake/MSVCCFlagOverride.cmake
@@ -1,21 +1,13 @@
 function(EnableMDFlag)
-    foreach(flag_var
-            CMAKE_CXX_FLAGS CMAKE_CXX_FLAGS_DEBUG CMAKE_CXX_FLAGS_RELEASE CMAKE_CXX_FLAGS_MINSIZEREL CMAKE_CXX_FLAGS_RELWITHDEBINFO
-            CMAKE_C_FLAGS CMAKE_C_FLAGS_DEBUG CMAKE_C_FLAGS_RELEASE CMAKE_C_FLAGS_MINSIZEREL CMAKE_C_FLAGS_RELWITHDEBINFO
-            )
-        if(${flag_var} MATCHES "/MT")
-            string(REGEX REPLACE "/MT" "/MD" ${flag_var} "${${flag_var}}")
-        endif(${flag_var} MATCHES "/MT")
-    endforeach(flag_var)
+    set(CMAKE_USER_MAKE_RULES_OVERRIDE
+            ${CMAKE_CURRENT_SOURCE_DIR}/c_flag_overrides_md.cmake)
+    set(CMAKE_USER_MAKE_RULES_OVERRIDE_CXX
+            ${CMAKE_CURRENT_SOURCE_DIR}/cxx_flag_overrides_md.cmake)
 endfunction()
 
 function(EnableMTFlag)
-    foreach(flag_var
-            CMAKE_CXX_FLAGS CMAKE_CXX_FLAGS_DEBUG CMAKE_CXX_FLAGS_RELEASE CMAKE_CXX_FLAGS_MINSIZEREL CMAKE_CXX_FLAGS_RELWITHDEBINFO
-            CMAKE_C_FLAGS CMAKE_C_FLAGS_DEBUG CMAKE_C_FLAGS_RELEASE CMAKE_C_FLAGS_MINSIZEREL CMAKE_C_FLAGS_RELWITHDEBINFO
-            )
-        if(${flag_var} MATCHES "/MD")
-            string(REGEX REPLACE "/MD" "/MT" ${flag_var} "${${flag_var}}")
-        endif(${flag_var} MATCHES "/MD")
-    endforeach(flag_var)
+    set(CMAKE_USER_MAKE_RULES_OVERRIDE
+            ${CMAKE_CURRENT_SOURCE_DIR}/c_flag_overrides_mt.cmake)
+    set(CMAKE_USER_MAKE_RULES_OVERRIDE_CXX
+            ${CMAKE_CURRENT_SOURCE_DIR}/cxx_flag_overrides_mt.cmake)
 endfunction()
