ece6528e6e907fce2034af35a7cd02a4626570f2
yangga <yangga0070@gmail.com>
2017-09-11 17:21:32 +0900

    test

--- tests/tests_base/test_logging/main.cpp
+++ tests/tests_base/test_logging/main.cpp
@@ -16,19 +16,10 @@ int main(int argc, char* argv[]) {
         std::string cfgPath(Zam_CMAKE_BUILT_PATH);
         cfgPath += "/misc/tests/test_logging/test_logging.json";
 
-#ifdef BOOST_WINDOWS_API
-        std::replace(cfgPath.begin(), cfgPath.end(), '/', '\\');
-#endif
-
         loggerSystem::load(cfgPath.c_str());
     } catch(std::exception& e) {
         std::cerr << "failed to load - " << e.what() << std::endl;
-
-        std::ofstream outFile("output.txt");
-        outFile << "failed to load - " << e.what() << std::endl;
-        outFile.close();
-
-        return 0;
+        return -1;
     }
 
     ZAM_LOGT("test1") << "trace";
