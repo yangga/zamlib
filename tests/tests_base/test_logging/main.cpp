9dcf1cdf7b5ed0ed7ea23a1202a49375703ac29d
yangga <yangga0070@gmail.com>
2017-09-11 16:52:12 +0900

    test

--- tests/tests_base/test_logging/main.cpp
+++ tests/tests_base/test_logging/main.cpp
@@ -6,6 +6,8 @@
 #include <zam/base/logger.h>
 #include <zam/base/filesystem/whereami.h>
 
+#include <boost/algorithm/string/replace.hpp>
+
 #include <iostream>
 
 int main(int argc, char* argv[]) {
@@ -16,27 +18,29 @@ int main(int argc, char* argv[]) {
         using namespace zam::base::log;
 
         std::string cfgPath(Zam_CMAKE_BUILT_PATH);
-        cfgPath += "C:\\projects\\zamlib\\misc\\tests\\test_logging\\test_logging.json";
+        cfgPath += "/misc/tests/test_logging/test_logging.json";
+
+#ifdef BOOST_WINDOWS_API
+        boost::replace_all(cfgPath, "/", boost::filesystem::path::preferred_separator);
+#endif
 
         loggerSystem::load(cfgPath.c_str());
     } catch(std::exception& e) {
-        std::cout << "failed to load - " << e.what() << std::endl;
-        return -1;
-    }
-//
-//    try {
-//        for (int i=0; i<10; ++i) {
-//            ZAM_LOGT("test1") << "trace";
-//            ZAM_LOGD("test1") << "debug";
-//            ZAM_LOGI("test1") << "info";
-//            ZAM_LOGW("test1") << "warning";
-//            ZAM_LOGE("test1") << "error";
-//            ZAM_LOGF("test1") << "fatal";
-//        }
-//    } catch(std::exception& e) {
-//        std::cerr << "failed to log - " << e.what() << std::endl;
-//    }
+        std::cerr << "failed to load - " << e.what() << std::endl;
 
+        std::ofstream outFile("output.txt");
+        outFile << "failed to load - " << e.what() << std::endl;
+        outFile.close();
+
+        return 0;
+    }
+
+    ZAM_LOGT("test1") << "trace";
+    ZAM_LOGD("test1") << "debug";
+    ZAM_LOGI("test1") << "info";
+    ZAM_LOGW("test1") << "warning";
+    ZAM_LOGE("test1") << "error";
+    ZAM_LOGF("test1") << "fatal";
 
     return 0;
 }
