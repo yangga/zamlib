04407dc55c460348de7b7f015d3e759b77de1390
yangga <yangga0070@gmail.com>
2017-09-11 16:11:35 +0900

    test

--- tests/tests_base/test_logging/main.cpp
+++ tests/tests_base/test_logging/main.cpp
@@ -10,30 +10,30 @@
 
 int main(int argc, char* argv[]) {
 
-    try {
-        using namespace zam::base::log;
-
-        std::string cfgPath(Zam_CMAKE_BUILT_PATH);
-        cfgPath += "/misc/tests/test_logging/test_logging.json";
-
-        loggerSystem::load(cfgPath.c_str());
-    } catch(std::exception& e) {
-        std::cerr << "failed to load - " << e.what() << std::endl;
-        return -1;
-    }
-
-    try {
-        for (int i=0; i<10; ++i) {
-            ZAM_LOGT("test1") << "trace";
-            ZAM_LOGD("test1") << "debug";
-            ZAM_LOGI("test1") << "info";
-            ZAM_LOGW("test1") << "warning";
-            ZAM_LOGE("test1") << "error";
-            ZAM_LOGF("test1") << "fatal";
-        }
-    } catch(std::exception& e) {
-        std::cerr << "failed to log - " << e.what() << std::endl;
-    }
+//    try {
+//        using namespace zam::base::log;
+//
+//        std::string cfgPath(Zam_CMAKE_BUILT_PATH);
+//        cfgPath += "/misc/tests/test_logging/test_logging.json";
+//
+//        loggerSystem::load(cfgPath.c_str());
+//    } catch(std::exception& e) {
+//        std::cerr << "failed to load - " << e.what() << std::endl;
+//        return -1;
+//    }
+//
+//    try {
+//        for (int i=0; i<10; ++i) {
+//            ZAM_LOGT("test1") << "trace";
+//            ZAM_LOGD("test1") << "debug";
+//            ZAM_LOGI("test1") << "info";
+//            ZAM_LOGW("test1") << "warning";
+//            ZAM_LOGE("test1") << "error";
+//            ZAM_LOGF("test1") << "fatal";
+//        }
+//    } catch(std::exception& e) {
+//        std::cerr << "failed to log - " << e.what() << std::endl;
+//    }
 
 
     return 0;
