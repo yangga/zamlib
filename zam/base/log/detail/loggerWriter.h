290551484d6db398a2ebb3fb1d16450d1233863a
Sungwoo Yang <yangga0070@gmail.com>
2017-09-12 13:35:45 +0900

    Fix win warning (#8)
    * fixed dll interface warning problems
    * suppressed msbuild info messages

--- zam/base/log/detail/loggerWriter.h
+++ zam/base/log/detail/loggerWriter.h
@@ -8,6 +8,7 @@
 #include <zam/base/base.h>
 #include <zam/base/log/loggerDefine.h>
 
+#include <boost/log/common.hpp>
 #include <boost/log/sinks.hpp>
 #include <boost/smart_ptr.hpp>
 
@@ -16,23 +17,31 @@ namespace zam {
     namespace base {
         namespace log {
 
-            class ZAMBASE_API loggerWriter {
+            class loggerWriter {
             public:
                 using sink_t = boost::shared_ptr<boost::log::sinks::sink>;
 
-                explicit loggerWriter(std::string name);
+                explicit loggerWriter(std::string name) : name_(std::move(name)) {}
 
                 inline const std::string& getName() const BOOST_NOEXCEPT { return name_; }
 
                 void setLevel(level lv) { lv_ = lv; }
 
-                void addSink(sink_t s);
+				void addSink(sink_t s) {
+					sinks_.push_back(s);
+				}
 
                 inline bool checkLevel(level lv) const BOOST_NOEXCEPT {
                     return (lv_ <= lv);
                 }
 
-                void flush(level lvContext, const char* txt);
+				void flush(level lvContext, const char* txt) {
+					namespace src = boost::log::sources;
+					namespace keywords = boost::log::keywords;
+
+					src::severity_channel_logger_mt<level> logger(keywords::channel = name_);
+					BOOST_LOG_SEV(logger, lvContext) << txt;
+				}
 
             private:
                 std::string name_;
