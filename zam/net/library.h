2c384c72012b8b7755613f7884ce409f7df7bfa1
yangga <yangga0070@gmail.com>
2017-09-17 09:30:53 +0900

    test

--- zam/net/library.h
+++ zam/net/library.h
@@ -1,6 +1,8 @@
 #ifndef NET_LIBRARY_H
 #define NET_LIBRARY_H
 
-void hello();
+#include <zam/net/net.h>
+
+ZAMNET_API void hello();
 
 #endif
--- zam/net/net.h
+++ zam/net/net.h
@@ -5,6 +5,27 @@
 #ifndef ZAM_STDAFX_H
 #define ZAM_STDAFX_H
 
+#include <boost/config.hpp>
+
+#ifdef BOOST_MSVC
+#ifndef _WINDOWS_
+#define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
+#include <windows.h>
+#include <Winsock2.h>
+#endif
+#endif
+
+#include <boost/assert.hpp>
 #include <boost/cstdint.hpp>
+#include <boost/filesystem.hpp>
+#include <boost/shared_ptr.hpp>
+
+#include <iostream>
+
+#ifdef zam_base_shared_EXPORTS
+#define ZAMNET_API BOOST_SYMBOL_EXPORT
+#else
+#define ZAMNET_API BOOST_SYMBOL_VISIBLE
+#endif
 
 #endif //ZAM_STDAFX_H
